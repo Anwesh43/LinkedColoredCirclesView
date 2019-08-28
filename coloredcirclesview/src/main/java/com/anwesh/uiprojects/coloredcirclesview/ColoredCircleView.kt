e1b531ef71d93b681d39bc2845663e60082fe021
Anwesh43 <anweshthecool0@gmail.com>
2019-08-28 10:45:42 +0530

    adding ColoredCircleView class

--- coloredcirclesview/src/main/java/com/anwesh/uiprojects/coloredcirclesview/ColoredCircleView.kt
+++ coloredcirclesview/src/main/java/com/anwesh/uiprojects/coloredcirclesview/ColoredCircleView.kt
@@ -16,3 +16,21 @@ import android.content.Context
 val scGap : Float = 0.05f
 val colors : Array<String> = arrayOf("#4527A0", "#0D47A1", "#f44336", "#00C853", "#E65100")
 val backColor : Int = Color.parseColor("#BDBDBD")
+
+class ColoredCircleView(ctx : Context) : View(ctx){
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
