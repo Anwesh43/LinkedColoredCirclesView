a5dfce746b03d9c487b76d37b87183f9ac817c2f
Anwesh43 <anweshthecool0@gmail.com>
2019-08-28 11:21:46 +0530

    using renderer in view

--- coloredcirclesview/src/main/java/com/anwesh/uiprojects/coloredcirclesview/ColoredCircleView.kt
+++ coloredcirclesview/src/main/java/com/anwesh/uiprojects/coloredcirclesview/ColoredCircleView.kt
@@ -20,15 +20,16 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 class ColoredCircleView(ctx : Context) : View(ctx){
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val renderer : Renderer = Renderer(this)
 
     override fun onDraw(canvas : Canvas) {
-
+        renderer.render(canvas, paint)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
